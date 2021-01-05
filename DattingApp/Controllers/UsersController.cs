bf2b12e9020b8b118b785707e0900e4ae7333ab6
Łukasz Stąpor <stapor.lukasz@hotmail.com>
2021-01-05 14:05:58 +0100

    new dotnet project

---
+++ DattingApp/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DattingApp.Data;
+using DattingApp.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DattingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Users
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+
+            return users;
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
