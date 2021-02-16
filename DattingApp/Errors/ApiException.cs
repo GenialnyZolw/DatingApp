45100a7d8c903ad8983dc3be66f7002a34a6cce5
Łukasz Stąpor <stapor.lukasz@hotmail.com>
2021-02-16 21:06:44 +0100

    Added api exception middleware

---
+++ DattingApp/Errors/ApiException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DattingApp.Errors
+{
+    public class ApiException
+    {
+    }
+}
