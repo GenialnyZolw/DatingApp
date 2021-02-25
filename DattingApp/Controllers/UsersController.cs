1e9be66a305977126521805f0c85324a8200e463
Łukasz Stąpor <stapor.lukasz@hotmail.com>
2021-02-25 19:55:10 +0100

    Updated users in api

--- DattingApp/Controllers/UsersController.cs
+++ DattingApp/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using DattingApp.Data;
@@ -43,18 +44,32 @@ namespace DattingApp.Controllers
             return user;
         }
 
+        // PUT: api/Users/5
+        [HttpPut()]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var userName = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await userRepository.GetUserByUserNameAsync(userName);
+
+            mapper.Map(memberUpdateDto, user);
+
+            userRepository.Update(user);
+
+            if (await userRepository.SaveAllAsync())
+            {
+                return NoContent();
+            }
+
+            return BadRequest("Failed to update user");
+        }
+
         // POST: api/Users
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT: api/Users/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
+ 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
